c9605778d6a4f7e05bd13b4a572260d40d61d6de
David Kebo <davidkebo@tamu.edu>
2023-05-02 01:44:34 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Project
+Instructions: https://docs.google.com/document/d/12t8HJU5uH1Zx0M33zzV9W_ydWwOcHFue39Br9_B9LIA/edit?usp=sharing
---
+++ zebu/run/libDPI/dpicalls.cc
@@ -0,0 +1,20 @@
+// © 2015 Synopsys, Inc.
+// This Synopsys ZeBu Lab source code and all associated documentation are proprietary and
+// confidential to Synopsys, Inc. and may only be used pursuant to the terms and conditions
+// of a written license agreement with Synopsys, Inc. All other use, reproduction,
+// modification, or distribution of the Synopsys ZeBu Lab source code or the associated
+// documentation is strictly prohibited.
+
+// Standard C libraries
+#include <svdpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+
+extern "C" void fifo_usage_spy_notify( )
+{
+ // TODO
+}
